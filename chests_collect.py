7975b37bffe13757454e5356e382462ae56f2b9b
Andrey <i78901234567890@gmail.com>
2020-01-23 14:13:01 +0300

    update. Бот переписан в ООП стиле с заделом под реализацию демона

--- chests_collect.py
+++ chests_collect.py
@@ -3,35 +3,27 @@ import time
 import numpy as np
 import pyautogui
 import pyscreenshot as sc
-from pynput.keyboard import Controller
-from pynput.mouse import Button, Controller as MouseController
-
-SCREEN = (1500, 1000, 1760, 1100)  # Место экрана которое будет обрабатываться
-CHEST = (1617, 1048)  # Место где находится сундук
-keyboard = Controller()
-mouse = MouseController()
-
-COLOR_FOR_FIND = np.array([0, 230, 203])  # цвет который будет искаться на месте обработки
-counter = 0
 
 
-if __name__ == '__main__':
-    while True:
-        time.sleep(60)
-        im = sc.grab(bbox=SCREEN)
+class Bot:
+    def __init__(self):
+        self.SCREEN = (1500, 1000, 1760, 1100)  # Место экрана которое будет обрабатываться
+        self.CHEST = (1617, 1048)  # Место где находится сундук
+        self.COLOR_FOR_FIND = np.array([0, 230, 203])  # цвет который будет искаться на месте обработки
+
+    def bot_execute(self):
+        counter = 0
+        im = sc.grab(bbox=self.SCREEN)
         array = np.array(im)
         for i in range(array.shape[0]):
             for j in range(array.shape[1]):
                 # если искомый пиксель есть в обрабатываемой области экрана, увеличиваем счетчик
-                if np.array_equal(array[i][j], COLOR_FOR_FIND):
+                if np.array_equal(array[i][j], self.COLOR_FOR_FIND):
                     counter += 1
-        print(counter)
         # если в обрабатываемой области достаточно искомых пикселей, перемещаем курсор в нужное места экрана
         # и производим клин
         if counter > 1000:
-            pyautogui.moveTo(*CHEST)
-            time.sleep(0.1)  # небольшие паузы перед действиями
-            mouse.click(Button.left, 1)
+            mouse_position = pyautogui.position()
+            pyautogui.click(*self.CHEST)
             time.sleep(0.1)
-            pyautogui.move(-200, -200)
-        counter = 0
+            pyautogui.moveTo(*mouse_position)
